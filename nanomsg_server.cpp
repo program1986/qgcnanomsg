3831ebf6caa40f0fcdf9fe61c1c1ab2081b492bb
shijingwei <shijingwei@inspiry.cn>
2023-10-11 16:52:40 +0800

    实现Publiser 功能

--- nanomsg_server.cpp
+++ nanomsg_server.cpp
@@ -2,18 +2,13 @@
  * @file nanomsg_server.cpp
  * @brief  QGC 服务模拟程序，用于调试。
  * @details
- * 本程序共建立两个服务。一个pubsub用于发布状态，一个req，用于接收命令。
+ * 本程序共建立两个服务。一个pubsub用于发布状态，一个req，用于接收命令。使用方法
+ * nanoserver -h 服务器地址 -p 发布端口  -c 命令端口
+ * 如：nanoserver -h 192.168.2.1 -p 5556 -c 5555
  * @author shijingwei
  * @version 0.1
  * @date 2023-10-05
  */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <iostream>
-#include <thread>
 
 #include <json/json.h>
 #include <nanomsg/bus.h>
@@ -21,7 +16,13 @@
 #include <nanomsg/pair.h>
 #include <nanomsg/pubsub.h>
 #include <nanomsg/reqrep.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+#include <iostream>
+#include <thread>
 
 using namespace std;
 
@@ -31,7 +32,42 @@ using namespace std;
 /**------------------数据定义-----------------------------------**/
 
 // 状态数据
-std::string statusString = R"(
+std::string statusString1 = R"(
+    {
+        "header": 1235,
+        "timestamp": 100000000,
+        "serial": 5,
+        "cmd": 1,
+        "data": {
+            "alarm_info": "",
+            "object_num": 2,
+            "object_array": [
+                {
+                    "object_id": 12,
+                    "object_name": "person",
+                    "object_conf": 0.99,
+                    "rect_color": "#ff0000",
+                    "rect_x": 400,
+                    "rect_y": 100,
+                    "rect_width": 200,
+                    "rect_height": 200
+                },
+                {
+                    "object_id": 4,
+                    "object_name": "car",
+                    "object_conf": 0.83,
+                    "rect_color": "#0000ff",
+                    "rect_x": 300,
+                    "rect_y": 300,
+                    "rect_width": 50,
+                    "rect_height": 50
+                }
+            ]
+        }
+    }
+)";
+
+std::string statusString2 = R"(
     {
         "header": 1235,
         "timestamp": 100000000,
@@ -46,27 +82,27 @@ std::string statusString = R"(
                     "object_name": "person",
                     "object_conf": 0.99,
                     "rect_color": "#ff0000",
-                    "rect_x": 0,
-                    "rect_y": 0,
-                    "rect_width": 100,
-                    "rect_height": 100
+                    "rect_x": 300,
+                    "rect_y": 300,
+                    "rect_width": 50,
+                    "rect_height": 50
                 },
                 {
                     "object_id": 4,
-                    "object_name": "car",
+                    "object_name": "dog",
                     "object_conf": 0.83,
-                    "rect_color": "#0000ff",
-                    "rect_x": 0,
-                    "rect_y": 0,
-                    "rect_width": 100,
-                    "rect_height": 100
+                    "rect_color": "#00ffff",
+                    "rect_x": 150,
+                    "rect_y": 150,
+                    "rect_width": 60,
+                    "rect_height": 60
                 }
             ]
         }
     }
 )";
 
-//回复指令
+// 回复指令
 std::string jsonReplay = R"(
 {
     "header": 1234,
@@ -88,6 +124,8 @@ std::string jsonReplay = R"(
  * @param url 发布服务地址
  */
 void createPubService(const char *url) {
+
+  int count =0;
   int sock = nn_socket(AF_SP, NN_PUB);
   if (sock < 0) {
     std::cerr << "Error creating socket: " << nn_strerror(nn_errno())
@@ -104,14 +142,19 @@ void createPubService(const char *url) {
   }
 
   while (true) {
-    int bytes = nn_send(sock, statusString.c_str(), statusString.size(), 0);
+    int bytes;
+    count ++;
+    if (count %2 ==0)
+      nn_send(sock, statusString1.c_str(), statusString1.size(), 0);
+    else
+      nn_send(sock, statusString2.c_str(), statusString2.size(), 0);
     if (bytes > 0) {
-      std::cout << "Sending success." << std::endl;
+      // std::cout << "Sending success." << std::endl;
     }
 
     // 按照一秒10次发送
     std::this_thread::sleep_for(
-        std::chrono::nanoseconds(100 * 1000));  // 每秒发送一次请求
+        std::chrono::seconds(2));  // 每秒发送一次请求
   }
 
   nn_close(sock);
@@ -119,143 +162,126 @@ void createPubService(const char *url) {
 
 /**
  * @brief 建立命令接收服务
- * 
+ *
  * @param url 服务地址
  */
 void createRespondService(const char *url) {
+  int sock = nn_socket(AF_SP, NN_REP);
+  if (sock < 0) {
+    std::cerr << "Error creating socket: " << nn_strerror(nn_errno())
+              << std::endl;
+    return;
+  }
 
-    int sock = nn_socket(AF_SP, NN_REP);
-    if (sock < 0) {
-        std::cerr << "Error creating socket: " << nn_strerror(nn_errno()) << std::endl;
-        return;
-    }
-
-    int bind_result = nn_bind(sock, url);
-    if (bind_result < 0) {
-        std::cerr << "Error binding socket: " << nn_strerror(nn_errno()) << std::endl;
-        nn_close(sock);
-        return;
-    }
-
-    while (true) {
-        char buf[BUF_LEN] = {0};
-        int bytes = nn_recv(sock, buf, sizeof(buf), 0);
-        if (bytes > 0) {
-            std::cout << "Received request: " << buf << std::endl;
-
-            std::string reply = jsonReplay;
-            int send_result = nn_send(sock, reply.c_str(), reply.size(), 0);
-            if (send_result < 0) {
-                std::cerr << "Error sending reply: " << nn_strerror(nn_errno()) << std::endl;
-            }
-        }
-    }
-
+  int bind_result = nn_bind(sock, url);
+  if (bind_result < 0) {
+    std::cerr << "Error binding socket: " << nn_strerror(nn_errno())
+              << std::endl;
     nn_close(sock);
-}
+    return;
+  }
 
-// 接收线程
-int cmd_recv_func(int sock) {
-  int rv;
-  char buf[BUF_LEN];
-  int bytes;
+  while (true) {
+    char buf[BUF_LEN] = {0};
+    int bytes = nn_recv(sock, buf, sizeof(buf), 0);
+    if (bytes > 0) {
+      std::cout << "Received request: " << buf << std::endl;
 
-  while (1) {
-    memset(buf, 0, BUF_LEN);
-    bytes = nn_recv(sock, &buf, BUF_LEN, 0);
-    if (bytes) {
-      printf("recieve client msg: %s\r\n", buf);
+      std::string reply = jsonReplay;
+      int send_result = nn_send(sock, reply.c_str(), reply.size(), 0);
+      if (send_result < 0) {
+        std::cerr << "Error sending reply: " << nn_strerror(nn_errno())
+                  << std::endl;
+      }
     }
   }
-  return (nn_shutdown(sock, rv));
-}
 
+  nn_close(sock);
+}
 
 int main(int argc, char **argv) {
   int server_sock = 0;
-  char buf[BUF_LEN] = {0};
   int option;
-  int port;
-  string hostAddress;
-  string connectionString;
 
- 
+  // 服务器地址
+  string hostAddress = "";
+  // 发布服务器端口
+  int publishPort = -1;
+  // 命令服务器端口
+  int commandPort = -1;
+
+  string publishConnectionString = "";
+  string commandConnectionString = "";
+
   // 检查命令行参数
   if (argc < 2) {
     hostAddress = "127.0.0.1";
-    port = 2021;
-    connectionString = "tcp://" + hostAddress + ":" + std::to_string(port);
-    cout << "Using the default connection:" << connectionString << endl;
-  }
+    publishPort = 5556;
+    commandPort = 5555;
 
-  while ((option = getopt(argc, argv, "h:p:")) != -1) {
-    switch (option) {
-      case 'h':
-        hostAddress = optarg;
-        break;
-      case 'p':
-        try {
-          port = std::stoi(optarg);
-        } catch (...) {
-          std::cout << "Invalid value for -p option." << std::endl;
+    commandConnectionString =
+        "tcp://" + hostAddress + ":" + std::to_string(commandPort);
+    publishConnectionString =
+        "tcp://" + hostAddress + ":" + std::to_string(publishPort);
+  } else {
+    while ((option = getopt(argc, argv, "h:c:p:")) != -1) {
+      switch (option) {
+        case 'h':
+          hostAddress = optarg;
+          break;
+        case 'p':
+          try {
+            publishPort = std::stoi(optarg);
+          } catch (...) {
+            std::cout << "Invalid value for -p option." << std::endl;
+            return 1;
+          }
+
+        case 'c':
+          try {
+            commandConnectionString = std::stoi(optarg);
+          } catch (...) {
+            std::cout << "Invalid value for -p option." << std::endl;
+            return 1;
+          }
+
+          break;
+        case '?':
+          std::cout << "Unknown option: " << optopt << std::endl;
           return 1;
-        }
-        break;
-      case '?':
-        std::cout << "Unknown option: " << optopt << std::endl;
-        return 1;
-    }
-  }
-
-  //url 获取
-  connectionString = "tcp://" + hostAddress + ":" + std::to_string(port);
-  cout << "Connection is " << connectionString << endl;
-
-  if ((server_sock = nn_socket(AF_SP, NN_PAIR)) < 0) {
-    cout << "Create server socket failed!" << endl;
-    return -1;
-  }
-
-  if (nn_bind(server_sock, connectionString.c_str()) < 0) {
-    cout << "Bind server sock failed!" << endl;
-    nn_close(server_sock);
-    return -1;
-  }
-
-  cout << "Server init success!" << endl;
-
-  // 启动接收线程
-  // std::thread cmd_thread{cmd_recv_func, server_sock};
-  std::thread pub_thread{createPubService, "tcp://127.0.0.1:5555"};
-
-  while (1) {
-    // printf("%s\n", json);
-
-    std::this_thread::sleep_for(
-        std::chrono::nanoseconds(1000 * 1000));  // 每秒发送一次请求
-
-    // std::string jsonInput;
-    // std::cin >> jsonInput;
-
-    // std::cout << jsonInput << std::endl;
-    //  if (jsonInput == "exit"){
-    //    break;
-    //  }
-
-    /*
-    if (jsonInput.empty() == false) {
-      if (nn_send(server_sock, jsonInput.c_str(), jsonInput.length(), 0) < 0) {
-        printf("send failed!\r\n");
-        nn_close(server_sock);
-        exit(EXIT_FAILURE);
       }
     }
-    */
   }
 
-  nn_close(server_sock);
-  // cmd_thread.native_handle();
-  // cout << "Service terminated normally." << endl;
+  if (commandConnectionString.empty() || publishConnectionString.empty()) {
+    //
+    if (hostAddress.empty() || (commandPort == -1) || (publishPort == -1)) {
+      std::cout
+          << "Operation failed. Please check the host address and port numbers."
+          << std::endl;
+      return -1;
+    }
+
+    commandConnectionString =
+        "tcp://" + hostAddress + ":" + std::to_string(commandPort);
+    publishConnectionString =
+        "tcp://" + hostAddress + ":" + std::to_string(publishPort);
+  }
+  // Start the command service thread
+  cout << "Starting the command service..." << endl;
+  std::thread cmd_thread{createRespondService, commandConnectionString.c_str()};
+
+  // Start the publish service thread
+  cout << "Starting the publish service..." << endl;
+  std::thread pub_thread{createPubService, publishConnectionString.c_str()};
+
+  cout << "Service start completed successfully." << endl;
+
+  while (1) {
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    cout << "Service is running..." << endl;
+  }
+
   return 0;
 }
 
@@ -263,7 +289,7 @@ int main(int argc, char **argv) {
 
 /**
  * @brief 测试json解析函数
- * 
+ *
  */
 void jsonExtraExample() {
   const std::string json = R"(
@@ -296,7 +322,7 @@ void jsonExtraExample() {
 
 /**
  * @brief Json 生成测试
- * 
+ *
  */
 void jsonGenerateExample() {
   Json::Value root;
